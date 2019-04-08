524fbf4e131f60f27e4bb26cec68f029c904a7e2
Noah <savedream75@gmail.com>
2019-04-09 01:11:01 +0900

    #4.2 React Router
    인스타그램에서 사용하는 페이지를 생성한다
    prop-types를 추가한다
    Router 설정 방식을 학습한다
    로그인 / 비로그인 상태 페이지를 연결한다

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,9 +2,13 @@ import React, { Component } from "react";
 import { ThemeProvider } from "styled-components";
 import GlobalStyled from "../Styles/GlobalStyled";
 import Theme from "../Styles/Theme";
+import Router from "./Router";
 
 export default () => (
 	<ThemeProvider theme={Theme}>
+	<>
 		<GlobalStyled />
+		<Router isLoggedIn={false} />
+	</>
 	</ThemeProvider>
 );
